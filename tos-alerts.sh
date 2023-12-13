83c119f9101e848824a636bd44d532c1f8148b13
Pat <pwoods@linode.com>
2023-12-13 12:58:16 -0500

    adjusted script to actually work for me, with some code commented for record keeping

--- tos-alerts.sh
+++ tos-alerts.sh
@@ -19,15 +19,15 @@ LINODE_TOKEN=$(cat "LINODE_TOKEN")
 # one_hour_ago=$(date -u -d '1 hour ago' +"%Y-%m-%dT%H:%M:%S")
 # For MacOS:
 
+# one_hour_ago=$(date -v '1 hour ago' +"%Y-%m-%dT%H:%M:%S")
 one_hour_ago=$(date -v -1H +"%Y-%m-%dT%H:%M:%S")
 
 # Make the API call and capture the response
 
 api_response=$(curl -s -H "Authorization: Bearer $LINODE_TOKEN" "https://api.linode.com/v4/support/tickets" -H "X-Filter: { \"opened\": { \"+gte\": \"$one_hour_ago\" } }")
 
-# Extract and print the summary field
+# Extract the summary from the first element in the "data" array (assuming there is only one)
 summary=$(echo "$api_response" | jq -r '.data[0].summary')
-echo "Summary: $summary"
 
 # Check if the summary includes the specific text
 
@@ -36,3 +36,4 @@ if [[ "$summary" == *"ToS Violation"* ]]; then
 else
   echo "No new tickets with subject line starting with 'ToS Violation'."
 fi
+
